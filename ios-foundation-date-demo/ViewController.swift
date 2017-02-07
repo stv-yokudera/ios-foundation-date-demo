0732300ca906e38d283a109b07ca243a02e1da7b
k-moto <k.motoyama@smartdt.jp>
2017-02-08 06:00:58 +0900

    初版

---
+++ ios-foundation-date-demo/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  ios-foundation-date-demo
+//
+//  Created by 本山功 on 2017/02/07.
+//  Copyright © 2017年 k_moto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    private var date = Date()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupInit()
+        print(date)
+        self.renderTimeIntervalSince()
+    }
+    
+    private func setupInit(){
+        date = Date.init()
+    }
+
+    // 現在日付に時間を追加する
+    private func renderAddTimeInterval(){
+        date.addTimeInterval(60 * 60 * 24)
+        print(date)
+    }
+    
+    // 現在日付に時間を追加し、追加した時間を戻す
+    private func renderAddingTimeInterval(){
+        print(date.addingTimeInterval(60 * 60 * 24 * 2))
+    }
+    
+    // 引数で渡した日付が過去か未来かを列挙型で取得する
+    private func renderCompare(){
+        var otherDate = Date()
+        otherDate.addTimeInterval(60 * 60 * 24)
+
+        print(date.compare(otherDate).rawValue)
+    }
+    
+    private func renderDescription(){
+//        var locale = Locale(identifier: "en_US")
+//        print(date.description(locale))
+    }
+    
+    // 引数で渡した日付との差分を秒で取得する
+    private func renderTimeIntervalSince(){
+        var otherDate = Date()
+        otherDate.addTimeInterval(60 * 60 * 24)
+        print(date.timeIntervalSince(otherDate))
+    }
+    
+}
+
